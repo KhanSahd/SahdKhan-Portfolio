51a406b64ab080ce825fa14cfea81da950f8531c
Sahd Khan <sahdkhan@csus.edu>
2021-12-24 03:34:40 -0800

    Added flex and started projects list.

---
+++ script.js
@@ -0,0 +1,10 @@
+const psa = document.getElementById('PSA');
+const psaList = document.getElementById('PSA-list')
+
+psa.addEventListener('click', function() {
+    if (psaList.style.display === 'none'){
+        psaList.style.display = 'block';
+    } else {
+        psaList.style.display = 'none';
+    }
+});
