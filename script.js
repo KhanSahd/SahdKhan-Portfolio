8334ff774fcd5947bf18da169a722e5384f21208
Sahd Khan <03sahd@gmail.com>
2021-12-25 02:44:46 -0800

    Finished design and layout. need to make it responsive

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const psa = document.getElementById('PSA');
-const psaList = document.getElementById('PSA-list')
+const psalist = document.getElementById('psainnerList')
+const pfl = document.getElementById('PFL');
+const pflList = document.getElementById('pflinnerList');
+const odin = document.getElementById('odin');
+const odinList = document.getElementById('odininnerList');
 
 psa.addEventListener('click', function() {
-    if (psaList.style.display === 'none'){
-        psaList.style.display = 'block';
+    if (psalist.style.display === 'none'){
+        psalist.style.display = 'block';
     } else {
-        psaList.style.display = 'none';
+        psalist.style.display = 'none';
+    }
+});
+
+pfl.addEventListener('click', function(){
+    if (pflList.style.display === 'none'){
+        pflList.style.display = 'block';
+    } else {
+        pflList.style.display = 'none';
+    }
+});
+
+odin.addEventListener('click', function(){
+    if (odinList.style.display === 'none'){
+        odinList.style.display = 'block';
+    } else {
+        odinList.style.display = 'none';
     }
 });
